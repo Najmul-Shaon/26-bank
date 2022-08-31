182287ca9bbb1bb4b192189d9ed80232f74b4800
Najmul-Shaon <najmul.nh.shaon@gmail.com>
2022-09-01 00:10:03 +0600

    grid added

--- js/login.js
+++ js/login.js
@@ -6,6 +6,9 @@ document.getElementById('login').addEventListener('click', function () {
     const password = passwordField.value;
     console.log(password);
     if (email === 'abc@gmail.com' && password === 'abc') {
+
+        // js e etar maddhome another html page e jump kore. abar arekti way ace---------
+        // = location.href ='bank.html';
         window.location.href = 'bank.html';
     }
     else {
