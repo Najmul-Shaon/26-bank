6fd09893685bfe5283521561d0ec17bea53e7d1a
Najmul-Shaon <najmul.nh.shaon@gmail.com>
2022-09-04 00:18:12 +0600

    added

--- js/bank.js
+++ js/bank.js
@@ -1,7 +1,25 @@
 document.getElementById('deposite').addEventListener('click', function () {
     const depositeAmount = document.getElementById('deposite-amount');
-    const amount = depositeAmount.value;
+    const amountString = depositeAmount.value;
+    const amount = parseFloat(amountString);
     const depositGrandTotal = document.getElementById('deposit-grand-total');
-    depositGrandTotal.innerText = amount;
+    const newTotalAmountString = depositGrandTotal.innerText;
+    const newTotalAmount = parseFloat(newTotalAmountString);
+    const newDepositTotal = newTotalAmount + amount;
+    depositGrandTotal.innerText = newDepositTotal;
+
+    // set balance section
+
+    const grandTotalBalance = document.getElementById('grand-total-balance');
+    const grandTotalBalanceString = grandTotalBalance.innerText;
+    const grandTotalBalanceNumber = parseFloat(grandTotalBalanceString);
+    const newGrandTotalBalance = amount + grandTotalBalanceNumber;
+    grandTotalBalance.innerText = newGrandTotalBalance;
+
+
+    // button clear section 
     depositeAmount.value = '';
+
 })
+
+// withdrawn section 
