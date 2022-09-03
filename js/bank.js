74a4b02483a41e8807caec19f5f663bb8e33f16d
Najmul-Shaon <najmul.nh.shaon@gmail.com>
2022-09-03 14:10:12 +0600

    addded

---
+++ js/bank.js
@@ -0,0 +1,7 @@
+document.getElementById('deposite').addEventListener('click', function () {
+    const depositeAmount = document.getElementById('deposite-amount');
+    const amount = depositeAmount.value;
+    const depositGrandTotal = document.getElementById('deposit-grand-total');
+    depositGrandTotal.innerText = amount;
+    console.log(depositGrandTotal.innerText);
+})
